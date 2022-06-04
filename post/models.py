f2284b36b53e51bc8b63e84ec09f2eb58b9570ae
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-04 09:11:22 +0300

    This commit creates a models class

--- post/models.py
+++ post/models.py
@@ -1,4 +1,9 @@
+from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model)
+class Post(models.Model):
+    image = models.ImageField()
+    description = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    
