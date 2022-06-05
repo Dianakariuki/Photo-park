88476bd3d42809682e9a6ce6b8167bba96d3989b
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 12:54:10 +0300

    This commit  updates the class post in user

--- post/models.py
+++ post/models.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     image = models.ImageField()
+    title = models.CharField(max_length=50)
     description = models.TextField()
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts', null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     
     class Meta:
         '''
