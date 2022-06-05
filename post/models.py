3939437b9f85db4a08d0ef0f78294cb56c1184b6
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 13:03:02 +0300

    This commit creates a like model class

--- post/models.py
+++ post/models.py
@@ -96,6 +96,10 @@ class Follow(models.Model):
 
     def __str__(self):
         return self.follower
+    
+class Like(models.Model):
+	user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
 
 
     
