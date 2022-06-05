23513c1b1f1a06f8ab5d361f9df7e759c03be6d0
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 12:07:02 +0300

    This commit creates methods in the classes

--- post/models.py
+++ post/models.py
@@ -7,3 +7,50 @@ class Post(models.Model):
     description = models.TextField()
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     
+    class Meta:
+        '''
+        Class method to display images by date published
+        '''
+        ordering = ["-pk"]
+
+    def save_post(self):
+        '''
+        Method to save our images
+        '''
+        self.save()
+
+    def delete_post(self):
+        '''
+        Method to delete our images
+        '''
+        self.delete()
+
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Models):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(post__id=id)
+        return comments
+
+    def __str__(self):
+        return self.comment
+    class Meta:
+        ordering=["-pk"]
+
+
+    
