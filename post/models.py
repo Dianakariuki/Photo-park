11565d11c5249c90b5daed3660aa41e662c0185f
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 12:59:41 +0300

    This commit creates imports and creates a receiver

--- post/models.py
+++ post/models.py
@@ -1,13 +1,26 @@
 from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile', null=True)
     name=models.CharField(max_length=50)
     bio=models.TextField(max_length=500,blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Post(models.Model):
     image = models.ImageField()
