8519cd4306a7d6e43f81ee284f65192a8f5e3a92
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 12:55:48 +0300

    This commit creates a profile model class

--- post/models.py
+++ post/models.py
@@ -2,6 +2,13 @@ from email.mime import image
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile', null=True)
+    name=models.CharField(max_length=50)
+    bio=models.TextField(max_length=500,blank=True)
+
 class Post(models.Model):
     image = models.ImageField()
     title = models.CharField(max_length=50)
