a3e319f4598d740c14742c157bc3663a4eeec4c5
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-05 14:05:22 +0300

    This commit adds imports to the views

--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http.response import Http404
+from post.models import Post, Comment, Profile, Like, Follow
+from django.shortcuts import redirect, render
+from .forms import CreateUserForm, UploadImageForm, CommentForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
+import json
+from django.contrib.auth.models import User
